8d5cef10fa08d8ff6dd217efbb99ca5d1f0ff207
smile\smile <naren_k@live.com>
2018-10-05 00:20:17 +0545

    remove folder

--- wp-content/themes/Travel-child/taxonomy-accommodation_type.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * The Template for displaying accommodations in a accommodation_type category . Simply includes the archive template.
- */
-
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-trav_get_template( 'archive-accommodation.php' );
